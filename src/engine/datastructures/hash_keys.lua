e11b9abc6226bb1d8150b964f1079d914779b8c4
Connie Hilarides <conni_h@outlook.com>
2016-09-17 16:23:11 -0700

    Working on more types

--- src/engine/datastructures/hash_keys.lua
+++ src/engine/datastructures/hash_keys.lua
@@ -1,43 +1,137 @@
 local ffi = require("ffi")
 local bit = require("bit")
 
+ffi.cdef[[
+    void *malloc(size_t size);
+    void free(void *ptr);
+]]
+
+local C = ffi.C
+local ffi_new = ffi.new
+local ffi_string = ffi.string
+local ffi_istype = ffi.istype
+local ffi_gc = ffi.gc
+
 local int32 = {}
 local int32_mt = { __index = int32 }
 local int32_ct
 
 function int32:hash(hasher)
-    hasher:write_i32(self)
+    hasher:write_i32(self.value)
 end
+
+function int32_mt:__tostring()
+    return tostring(self.value)
+end
+
+function int32_mt.__eq(lhs, rhs)
+    return ffi_istype(int32_ct, lhs) and ffi_istype(int32_ct, rhs) and lhs.value == rhs.value
+end
+
+int32_ct = ffi.metatype("struct{int32_t value;}", int32_mt)
 
 local uint32 = {}
 local uint32_mt = { __index = uint32 }
 local uint32_ct
 
 function uint32:hash(hasher)
-    hasher:write_u32(self)
+    hasher:write_u32(self.value)
 end
+
+function uint32_mt:__tostring()
+    return tostring(self.value)
+end
+
+function uint32_mt.__eq(lhs, rhs)
+    return ffi_istype(uint32_ct, lhs) and ffi_istype(uint32_ct, rhs) and lhs.value == rhs.value
+end
+
+uint32_ct = ffi.metatype("struct{uint32_t value;}", uint32_mt)
 
 local int64 = {}
 local int64_mt = { __index = int64 }
 local int64_ct
 
 function int64:hash(hasher)
-    hasher:write_i64(self)
+    hasher:write_i64(self.value)
 end
+
+function int64_mt:__tostring()
+    return tostring(self.value)
+end
+
+function int64_mt.__eq(lhs, rhs)
+    return ffi_istype(int64_ct, lhs) and ffi_istype(int64_ct, rhs) and lhs.value == rhs.value
+end
+
+int64_ct = ffi.metatype("struct{int64_t value;}", int64_mt)
 
 local uint64 = {}
 local uint64_mt = { __index = uint64 }
 local uint64_ct
 
 function uint64:hash(hasher)
-    hasher:write_u64(self)
+    hasher:write_u64(self.value)
 end
+
+function uint64_mt:__tostring()
+    return tostring(self.value)
+end
+
+function uint64_mt.__eq(lhs, rhs)
+    return ffi_istype(uint64_ct, lhs) and ffi_istype(uint64_ct, rhs) and lhs.value == rhs.value
+end
+
+uint64_ct = ffi.metatype("struct{uint64_t value;}", uint64_mt)
 
 local string = {}
 local string_mt = { __index = string }
 local string_ct
 
+function string:hash(hasher)
+    hasher:write(self.data, self.len)
+end
 
+function string_mt.__new(tp, s, istemp)
+    if type(s) ~= 'string' then
+        error("Expected string")
+    end
+
+    if istemp then
+        return ffi_gc(ffi_new(string_ct, s, #s), nil)
+    else
+        local ptr = C.malloc(#s)
+        return ffi_new(string_ct, ptr, #s)
+    end
+end
+
+function string_mt:__gc()
+    C.free(self.data)
+end
+
+function string_mt:__tostring()
+    return ffi_string(self.data, self.len)
+end
+
+function string_mt.__eq(lhs, rhs)
+    if (not ffi_istype(string_ct, lhs)) or (not ffi_istype(string_ct, rhs)) then
+        return false
+    end
+
+    if lhs.len ~= rhs.len then
+        return false
+    end
+
+    for i = 0, lhs.len-1 do
+        if lhs.data[i] ~= rhs.data[i] then
+            return false
+        end
+    end
+
+    return true
+end
+
+string_ct = ffi.metatype("struct{const uint8_t *data; uint32_t len;}", string_mt)
 
 return {
     int32_t = int32_ct,
@@ -45,6 +139,5 @@ return {
     int64_t = int64_ct,
     uint64_t = uint64_ct,
 
-    string = string_ct,
-    string_dtor = string_mt.__gc
+    string = string_ct
 }
---
+++ src/engine/datastructures/vector.lua
@@ -0,0 +1,120 @@
+local ffi = require("ffi")
+
+ffi.cdef[[
+    void *malloc(size_t size);
+    void *realloc(void *ptr, size_t size);
+    void free(void *ptr);
+
+    void *memmove(void *dst, void *src, size_t len);
+]]
+
+local ffi_new = ffi.new
+local ffi_gc = ffi.gc
+local C = ffi.C
+
+local function build_type(value_t, needs_dtor)
+    local value_size = ffi.sizeof(value_t)
+
+    local vector_t = ffi.typeof([[
+        struct {
+            $ *data;
+            size_t cap;
+            size_t len;
+        }
+    ]], value_t)
+
+    local Vector = {}
+    local Vector_mt = { __index = Vector }
+    local Vector_ct
+
+    local function alloc(self, new_cap)
+        if new_cap == 0 then
+            C.free(self.data)
+            self.data = nil
+        elseif self.cap == 0 then
+            self.data = C.malloc(new_cap)
+            if self.data == nil then
+                error("malloc failed")
+            end
+        else
+            local new_data = C.realloc(self.data, new_cap)
+            if new_data == nil then
+                error("realloc failed")
+            end
+            self.data = new_data
+        end
+        self.cap = new_cap
+    end
+
+    local function grow(self, required)
+        local cap = self.cap * 2
+        if cap < required then
+            cap = required
+        end
+        alloc(self, cap)
+    end
+
+    function Vector_mt.__new(tp, cap)
+        local vec = ffi_new(tp, nil, 0, 0)
+        if cap and cap > 0 then
+            alloc(vec, cap)
+        end
+        return vec
+    end
+
+    if needs_dtor then
+        function Vector_mt:__gc()
+            for i, v in ipairs(self) do
+                ffi_new(value_t, v)
+            end
+            alloc(self, 0)
+        end
+    else
+        function Vector_mt:__gc()
+            alloc(self, 0)
+        end
+    end
+
+    local function iter(vec, i)
+        i = i + 1
+        if i >= vec.len then
+            return nil
+        end
+        return i, vec:get(i)
+    end
+
+    function Vector_mt:__ipairs()
+        return iter, self, 0
+    end
+
+    function Vector:push(value)
+        if self.len == self.cap then
+            grow(self, self.len + 1)
+        end
+        self.data[self.len] = ffi_gc(value, nil)
+        self.len = self.len + 1
+    end
+
+    function Vector:pop()
+        if self.len == 0 then
+            return nil
+        end
+        self.len = self.len - 1
+        return ffi_new(value_t, self.data[self.len])
+    end
+
+    function Vector:get(i)
+        if i < 1 or i > self.len then
+            return nil
+        end
+        return self.data[i - 1]
+    end
+
+    function Vector:remove(i)
+        if i < 1 or i > self.len then
+            return nil
+        end
+        local tmp = ffi_new(value_t, self.data[i - 1])
+        
+    end
+end
